fa2173f2659bde92207c2cf423b00cfe1d64aea9
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-08-19 09:13:28 +0200

    firstCommit

---
+++ routes/fruitRouter.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {getFruits, getFruit, insertFruit, deleteFruit, updateFruit} from '../controller/fruitController.js'
+const router = express.Router()
+import { verifyAToken } from '../middleware/authenticate.js'
+
+router.post('/cart',verifyAToken,(req,res)=>{
+    console.log(req.body);
+    
+    res.json({message:"You've added an item to cart"})
+})
+router.
+    route('/')
+        .get(getFruits)
+        .post(insertFruit)
+        
+router.
+    route('/:id')
+        .get(getFruit)
+        .delete(deleteFruit)
+        .patch(updateFruit)
+
+export default router
